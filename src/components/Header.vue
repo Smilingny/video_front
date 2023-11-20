73571b6725ffe7fb4c4c7c7ab13147aa59006be9
hebingxing <2545530864@qq.com>
2023-11-20 23:49:07 +0800

    1120

---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<script setup>
+
+
+import {reactive, toRefs} from "vue";
+import {Message, Search} from "@element-plus/icons-vue";
+
+const state = reactive({
+    url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+})
+
+const { url } = toRefs(state)
+
+</script>
+
+<template>
+
+<header>
+  <!--头部部分-->
+  <div class="header-part_0">
+    <el-avatar  :size="28"  :src="url" />
+  </div>
+  <!--  搜索框部分-->
+  <div class="header-part_1">
+    <el-input   placeholder="搜索" :prefix-icon="Search" ></el-input>
+
+  </div>
+  <!-- 信封部分-->
+  <div class="header-part_2">
+    <el-icon color="white"  size="25"><Message /></el-icon>
+  </div>
+</header>
+<!--文字显示部分-->
+</template>
+
+<style scoped>
+header{
+  display: flex;
+  width: 100vw;
+  height: 12vw;
+  background-color: aqua;
+  justify-content: space-between;
+  align-items: center;
+}
+header .header-part_0{
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 8vw;
+  width: 10vw;
+}
+header .header-part_1{
+  flex: 7;
+  height: 8vw;
+  width: 10vw;
+  background-color: aquamarine;
+  margin-right: 2vw;
+}
+header .header-part_2{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 2;
+  height: 8vw;
+  width: 10vw;
+
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <Header/>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
