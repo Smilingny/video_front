57617d952d47d38e6e1c4422bd2ec0cce7a0ebaa
hebingxing <2545530864@qq.com>
2023-11-27 19:25:28 +0800

    增加了视频上传模块的简单页面样式(修改）

--- src/router/index.js
+++ src/router/index.js
@@ -4,47 +4,43 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'index',
-    //   component: () => import('../views/IndexView.vue'),
-    //   children: [
-    //     {
-    //       path: '/',
-    //       name: 'home',
-    //       component: () => import('../views/HomeView.vue')
-    //     }
-    //   ]
-    // },
-    // {
-    //   path: '/videoDetail',
-    //   name: 'videoDetail',
-    //   component: () => import('../views/VideoDetailView.vue')
-    // },
-    // {
-    //   path: '/login',
-    //   name: 'login',
-    //   component: () => import('../views/LoginView.vue')
-    // },
-    // {
-    //   path: '/register',
-    //   name: 'register',
-    //   component: () => import('../views/Register.vue')
-    // },
-    // {
-    //   path:'/info_0',
-    //   name:'info_0',
-    //   component:()=>import('../views/info_0.vue')
-    // },
     {
-      path:'/',
-      name:'test',
-      component:()=>import('../views/UploadVideo.vue')
+      path: '/',
+      name: 'index',
+      component: () => import('../views/IndexView.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('../views/HomeView.vue')
+        }
+      ]
     },
     {
-      path:'/1',
+      path: '/videoDetail',
+      name: 'videoDetail',
+      component: () => import('../views/VideoDetailView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Register.vue')
+    },
+    {
+      path:'/info_0',
+      name:'info_0',
+      component:()=>import('../views/info_0.vue')
+    },
+
+    {
+      path:'/loadVideo',
       name: 'test1',
-      component:()=>import('../views/UploadVideo_0.vue')
+      component:()=>import('../views/UploadVideo.vue')
 
     }
   ]
--- src/views/UploadVideo.vue
+++ src/views/UploadVideo.vue
@@ -1,7 +1,8 @@
 <script setup>
 
 import Header from "@/components/Header.vue";
-import {Plus} from "@element-plus/icons-vue";
+import {Plus, UploadFilled} from "@element-plus/icons-vue";
+import VideoCard from "@/components/VideoCard.vue";
 const  url = 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
 </script>
 
@@ -9,42 +10,71 @@ const  url = 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg
   <div class="wrapper">
     <!--头部-->
     <div class="header">
-<!--    <span style="left: 0;position: absolute">-->
-<!--      <i class="fas fa-angle-left"></i>-->
-<!--    </span>-->
+    <span style="left: 0;position: absolute;margin-left: 1vw;color: white;font-size: 8vw">
+      <i class="fas fa-angle-left"></i>
+    </span>
       <div class="title">
         <span>上传视频</span>
       </div>
     </div>
-    <!--  视频相关-->
-<!--    <div class="select">-->
-<!--      <div class="left" style="flex: 1;margin-left: 4vw">-->
-<!--        <el-image style="width: 35vw; height: 35vw" :src="url" :fit="fit" />-->
-<!--      </div>-->
-<!--      <div class="right" style="flex: 1">-->
-<!--        <el-upload list-type="picture-card"  >-->
-<!--          <el-icon><Plus /></el-icon>-->
-<!--        </el-upload>-->
-<!--      </div>-->
 
-<!--    </div>-->
-<!--    &lt;!&ndash;文字部分的&ndash;&gt;-->
-<!--    <div>-->
-<!--      <p style="margin-left: 2vw">描述信息</p>-->
-<!--      <el-form>-->
-<!--        <el-form-item label="简介" prop="region" style="margin-left: 2vw">-->
-<!--          <el-input  type="textarea" style="width: 70vw"/>-->
-<!--        </el-form-item>-->
-<!--      </el-form>-->
-<!--    </div>-->
-    <!--底部-->
-<!--    <div class="bottom">-->
-<!--      &lt;!&ndash;取消按钮&ndash;&gt;-->
-<!--      <el-button>取消</el-button>-->
-<!--      &lt;!&ndash;上传&ndash;&gt;-->
-<!--      <el-button>确定</el-button>-->
+    <div class="middle">
+      <!--视频相关-->
+      <div class="videoShow" style="margin-top: 2vw">
+        <el-upload
+            class="upload-demo"
+            drag
+            action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+            multiple
+        >
+          <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+          <div class="el-upload__text">
+            拖拽视频或 <em>点击上传</em>
+          </div>
+          <template #tip>
+            <div class="el-upload__tip">
 
-<!--    </div>-->
+            </div>
+          </template>
+        </el-upload>
+      </div>
+
+      <video  style="width: 100vw;height: 20vw" autoplay>
+        <source src="../../public/ynu.mp4" type="video/mp4" />
+
+      </video>
+      <!--封面相关-->
+      <div class="select">
+
+
+        <div class="left" style="flex: 1;">
+          <el-image style="width: 35vw; height: 25vw;margin-left: 5vw" :src="url" :fit="fit" />
+        </div>
+        <div class="right" style="flex: 1">
+          <el-upload list-type="picture-card"  >
+            <el-icon><Plus /></el-icon>
+          </el-upload>
+        </div>
+      </div>
+
+      <div>
+        <p style="margin-left: 2vw">描述信息</p>
+        <el-form>
+          <el-form-item label="简介" prop="region" style="margin-left: 2vw">
+            <el-input  type="textarea" style="width: 70vw"/>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+
+
+    <div class="bottom">
+      <!--取消按钮-->
+      <el-button style="margin-left: 2vw" size="large" type="danger" round>取消</el-button>
+      <!--上传-->
+      <el-button style="margin-right: 2vw" size="large" type="success" round>确定</el-button>
+
+    </div>
 </div>
 
 </template>
@@ -53,6 +83,8 @@ const  url = 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg
 .wrapper{
   width: 100%;
   height: 100%;
+  box-sizing: border-box;
+
 }
 .wrapper .header {
   width: 100vw;
@@ -63,25 +95,17 @@ const  url = 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg
   justify-content: center;
 }
 .wrapper .header .title{
-
   color: white;
   font-size: 6vw;
 }
-.wrapper header span i {
-  margin-left: 2vw;
-  color: white;
-  font-size: 8vw;
-}
 .wrapper .select{
-  width: 100vw;
-  height: 40vw;
+  margin-top: 3vw;
   display: flex;
-  margin-top: 2vw;
-  margin-left: 2vw;
-  align-items: center;
   justify-content: space-between;
+  align-items: center;
+  height: 40vw;
 }
-.bottom {
+.bottom{
   width: 100vw;
   position: fixed;
   bottom: 0;
@@ -90,5 +114,12 @@ const  url = 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg
   display: flex;
   align-items: center;
   justify-content: space-between;
+  height: 20vw;
+}
+.wrapper .middle{
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  margin-bottom: 20vw;
 }
 </style>
