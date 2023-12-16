489628a95199dfc6ebc1c445bf79ed157e46316f
Smilingny <230502771@qq.com>
2023-12-16 11:33:17 +0800

    axios

--- src/router/index.js
+++ src/router/index.js
@@ -3,48 +3,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: () => import('../views/IndexView.vue'),
-      children: [
-        {
-          path: '/',
-          name: 'home',
-          component: () => import('../views/HomeView.vue')
-        }
-      ]
-    },
-    {
-        path: '/videoDetail',
-        name: 'videoDetail',
-        component: () => import('../views/VideoDetailView.vue'),
-        children: [
-            {
-                path: '/commentDetail',
-                name: 'commentDetail',
-                component: () => import('../components/video/CommentDetail.vue')
-            }
-        ]
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/Register.vue')
-    },
-      /*个人信息展示界面*/
-    {
-      path:'/info',
-      name:'info',
-      component:()=>import('../views/Info.vue')
-    },
-    history: createWebHistory(import.meta.env.BASE_URL),
+
     routes: [
         {
             path: '/',
